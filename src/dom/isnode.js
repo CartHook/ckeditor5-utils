b1a8ef51bfafdf92f0776f5f1d34006368b2bdea
Jaša Mihelčič <jasa.mihelcic@spartan.si>
2019-01-14 12:17:08 +0100

    Implement grapesjs specific isNode function

--- src/dom/isnode.js
+++ src/dom/isnode.js
@@ -9,18 +9,41 @@
 
 /**
  * Checks if the object is a native DOM Node.
+ * This version is specific to usage with grapesjs
  *
  * @param {*} obj
  * @returns {Boolean}
  */
 export default function isNode( obj ) {
 	if ( obj ) {
-		if ( obj.defaultView ) {
+		if (obj.defaultView) {
+			if (obj.defaultView.frameElement != null) {
+				if (obj.defaultView.frameElement.className.includes('gjs-frame')) {
+					return obj instanceof obj.defaultView.Document;
+				}
+			}
 			return obj instanceof obj.defaultView.Document;
-		} else if ( obj.ownerDocument && obj.ownerDocument.defaultView ) {
+		} else if (obj.ownerDocument && obj.ownerDocument.defaultView) {
+			if (obj.ownerDocument.defaultView.frameElement != null) {
+				if (obj.ownerDocument.defaultView.frameElement.className.includes('gjs-frame')) {
+					return obj instanceof obj.ownerDocument.defaultView.frameElement.ownerDocument.defaultView.Node;
+				}
+			}
 			return obj instanceof obj.ownerDocument.defaultView.Node;
 		}
 	}
 
 	return false;
 }
+
+// export default function isNode( obj ) {
+// 	if ( obj ) {
+// 		if ( obj.defaultView ) {
+// 			return obj instanceof obj.defaultView.Document;
+// 		} else if ( obj.ownerDocument && obj.ownerDocument.defaultView ) {
+// 			return obj instanceof obj.ownerDocument.defaultView.Node;
+// 		}
+// 	}
+//
+// 	return false;
+// }
